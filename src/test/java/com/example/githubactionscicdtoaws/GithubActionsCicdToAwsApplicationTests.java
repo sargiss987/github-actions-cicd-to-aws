b467b309c5feac78c4ebb4a19a52d04a0aa44673
Sargis Sargsyan <sargiss987@gmail.com>
2022-02-17 18:37:37 +0400

    inital commit

---
+++ src/test/java/com/example/githubactionscicdtoaws/GithubActionsCicdToAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionscicdtoaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsCicdToAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
